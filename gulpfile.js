444252287ac3c9f1aeca04c0303b991fa8820e2a
Matt Donnelly <mattdonnelly@me.com>
2016-07-01 23:39:58 +0100

    Update gulpfile + build folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,28 +2,30 @@ const gulp = require('gulp')
 const less = require('gulp-less')
 
 gulp.task('less', function () {
-  gulp.src('src/less/main.less')
+  gulp.src('less/main.less')
     .pipe(less())
-    .pipe(gulp.dest('css'))
-})
-
-gulp.task('watch-less', function () {
-  gulp.watch('src/less/*.less', ['less'])
-})
-
-gulp.task('watch-js', function () {
-  gulp.watch('src/js/*.js', ['js'])
+    .pipe(gulp.dest('build/css'))
 })
 
 gulp.task('bundle', function () {
   gulp.src(['style.css', 'screenshot.png', '*.php']).pipe(gulp.dest('build'))
   gulp.src('img/*.{svg,png,jpg,gif}').pipe(gulp.dest('build/img'))
-  gulp.src('css/*.css').pipe(gulp.dest('build/css'))
   gulp.src('inc/*.php').pipe(gulp.dest('build/inc'))
   gulp.src('template-parts/*.php').pipe(gulp.dest('build/template-parts'))
 })
 
 gulp.task('build', ['less', 'bundle'])
 
-gulp.task('watch', ['watch-less'])
+gulp.task('watch', function () {
+  gulp.watch([
+    'style.css',
+    'screenshot.png',
+    '*.php',
+    'img/*',
+    'src/**/*',
+    'inc/**/*',
+    'template-parts/**/*'
+  ], ['build'])
+})
+
 gulp.task('default', ['build'])
